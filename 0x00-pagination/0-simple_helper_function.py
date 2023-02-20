b0fefaac4e8fe1839831587b676f0cbbd7fb39b2
root <amzid@hotmail.fr>
2023-02-20 22:36:35 +0000

    Task0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+ Simple helper function
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    '''
+    Function that returns a start index and an end index
+    '''
+
+    return ((page - 1) * page_size, page * page_size)
